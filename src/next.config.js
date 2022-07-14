28f309e55e3dd4a39d78692bf3c47694ac019b7b
Seiji Tanaka <nsy999jp@gmail.com>
2022-07-14 11:57:10 +0900

    Fix

--- src/next.config.js
+++ src/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['d3d1owzsxs2d42.cloudfront.net', 'placehold.jp']
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/about2.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function About(props) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SSG</title>
+        <meta name="description" content="ただのAboutページだよ" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>おしゃれな食器とか（SSG）</h1>
+        <div className={styles.parent}>
+        {props.products.map((product)=>{
+          return (
+            <div className={styles.child}>
+              <Image src={product.image} width={150} height={150} />
+            </div>
+          )
+        })}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps (context) {
+  const products = await fetch(`${process.env.URL}`).then(data => data.json());
+  return {
+    props: {
+      products,
+    }
+  }
+}
